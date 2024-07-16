d3af44f5bf883e9bbf52de9b82f5746c9098cfbc
jeikhan <evgenii.kapaev@yandex.ru>
2024-07-16 21:53:46 +0300

    5. Tracker - хранилище [#396 #5096]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,40 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
+/**
+ * Консольное приложение Tracker. Модель заявки.
+ *
+ * @author Evgeniy Kapaev
+ */
 public class Item {
+
+    /**
+     * Уникальный номер заявки.
+     */
     private int id;
+
+    /**
+     * Содержит название заявки.
+     */
     private String name;
+
+    /**
+     * Содержит дату создания заявки
+     */
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId() {
         return id;
@@ -18,5 +50,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,25 +1,84 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
+/**
+ * Консольное приложение Tracker.
+ *
+ * @author Evgeniy Kapaev
+ */
 public class Tracker {
+
+    /**
+     * Массив заявок.
+     */
     private final Item[] items = new Item[100];
+
+    /**
+     * Уникальный номер добавленной заявки.
+     */
     private int ids = 1;
+
+    /**
+     * Размер массива заявок.
+     */
     private int size = 0;
 
+    /**
+     * Добавляет заявку, переданную в аргументах в массив заявок items.
+     *
+     * @param item заявка.
+     * @return добавленная заявка.
+     */
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
+    /**
+     * Возвращает копию массива items без null элементов(без пустых ячеек).
+     *
+     * @return копия массива items.
+     */
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    /**
+     * Вывод заявок, у которых имя совпадает с аргументом(key)
+     * для поиска.
+     *
+     * @param key аргумент для поиска заявок по имени.
+     * @return результирующий массив заявок.
+     */
+    public Item[] findByName(String key) {
+        Item[] result = new Item[this.size];
+        int localSize = 0;
+        for (int i = 0; i < this.size; i++) {
+            if (items[i].getName().equals(key)) {
+                result[localSize++] = items[i];
+            }
+        }
+        return Arrays.copyOf(result, localSize);
+    }
+
+    /**
+     * Вывод заявок, у которых id совпадает с аргументом(id)
+     * для поиска.
+     *
+     * @param id аргумент для поиска заявок по id.
+     * @return элемент массива заявок, соответствующий аргументу.
+     */
     public Item findById(int id) {
-        Item rsl = null;
+        Item result = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
-                rsl = item;
+                result = item;
                 break;
             }
         }
-        return rsl;
+        return result;
     }
 }
