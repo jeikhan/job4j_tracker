0612b99e5ad5423c189f929c6855f12c8c00de0f
jeikhan <evgenii.kapaev@yandex.ru>
2024-07-17 11:46:41 +0300

    3.0. Реализация класса StartUI. Вывод меню [#500743 #168144]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,54 @@
+package ru.job4j.tracker;
+
+import java.util.Scanner;
+
+/**
+ * Консольное приложение для работы с классом ru.job4j.tracker.Tracker.
+ *
+ * @author Evgeniy Kapaev
+ */
+public class StartUI {
+
+    /**
+     * Инициализирует приложение и запускает выполнение различных
+     * пользовательских действий.
+     */
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Выбрать: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    /**
+     * Выводит на экран меню доступных пользовательских действий.
+     */
+    private void showMenu() {
+        String[] menu = {
+                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
+                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
+                "Завершить программу"
+        };
+        System.out.println("Меню:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
+    /**
+     * Запускает приложение.
+     * @param args аргументы командной строки.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+    }
+}
