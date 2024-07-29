093c0a0f6932be79c70230f97f83b19d3cc9bd7e
jeikhan <evgenii.kapaev@yandex.ru>
2024-07-29 12:15:00 +0300

    8. Input и полиморфизм. [#181094 #169288]

--- src/main/java/ru/job4j/tracker/MockInput.java
+++ src/main/java/ru/job4j/tracker/MockInput.java
@@ -1,14 +1,24 @@
 package ru.job4j.tracker;
 
+/**
+ * Класс-реализация интерфейса Input для моделирования
+ * ввода данных пользователем.
+ */
 public class MockInput implements Input {
+    private String[] answers;
+    private int position = 0;
+
+    public MockInput(String[] answers) {
+        this.answers = answers;
+    }
 
     @Override
     public String askStr(String question) {
-        return null;
+        return answers[position++];
     }
 
     @Override
     public int askInt(String question) {
-        return 0;
+        return Integer.parseInt(askStr(question));
     }
 }
